1c1f926be3925bc5297456778a9b8b21d75a83da
Deyan Grigorov <deian1998@abv.bg>
2021-06-17 13:24:20 +0300

    add new models and views

--- petstagram_django/pets/admin.py
+++ petstagram_django/pets/admin.py
@@ -5,7 +5,10 @@ from petstagram_django.pets.models import Pets
 
 
 class PetsAdmin(admin.ModelAdmin):
-    list_display = ['name', 'type', 'age']
+    list_display = ['name', 'type', 'age', 'likes_count']
+
+    def likes_count(self, obj):
+        return obj.like_set.count()
 
 
 admin.site.register(Pets, PetsAdmin)
--- petstagram_django/pets/views.py
+++ petstagram_django/pets/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from petstagram_django.pets.models import Pets, Like
+
+
+def list_pets(request):
+    all_pets = Pets.objects.all()
+
+    context = {
+        'pets': all_pets
+    }
+
+    return render(request, 'pets/pet_list.html', context)
+
+
+def pet_details(request, pk):
+    pet = Pets.objects.get(pk=pk)
+    pet.likes_count = pet.like_set.count
+
+    context = {
+        'pet': pet
+    }
+
+    return render(request, 'pets/pet_detail.html', context)
+
+
+def like_pet(request, pk):
+    pet = Pets.objects.get(pk=pk)
+    like = Like(
+        pet=pet,
+    )
+    like.save()
+    return redirect('list pets')
